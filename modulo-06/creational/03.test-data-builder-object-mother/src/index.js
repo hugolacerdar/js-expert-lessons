8ec9c605affe8cbe2304659b641e32c7f41ff7e8
Hugo <hugolacerdar@gmail.com>
2021-04-06 19:57:37 -0300

    Module 6, Lesson 3 - Complete

---
+++ modulo-06/creational/03.test-data-builder-object-mother/src/index.js
@@ -0,0 +1,35 @@
+/*
+ProductId: should be btwn 2 and 20 chars
+Name: should be only words
+Price: should be from zero to 1000
+Category: should be eletronic or organic
+*/  
+
+function productValidator(product) {
+    const errors = [];
+
+    if(!(product.id.length >= 2 && product.id.length <= 20)) {
+        errors.push(`id: invalid length, current [${product.id.length}] expected to be between 2 and 20`);
+    }
+
+    if(/(\W|\d)/.test(product.name)) {
+        errors.push(`name: invalid value, current [${product.name}] expected to have only words`);
+    }
+
+    if(!(product.price >= 1 && product.price <= 1000)) {
+        errors.push(`price: invalid value, current [${product.price}] expected to be between 1 and 1000`);
+    }
+
+    if(!(['electronic', 'organic'].includes(product.category))) {
+        errors.push(`category: invalid value, current [${product.category}] expected to be either electronic or organic`);
+    }
+
+    return {
+        result: errors.length === 0,
+        errors
+    }
+} 
+
+module.exports = {
+    productValidator
+}
